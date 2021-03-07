b74c9fbc5b0fb37a8010d9392edd33c3cd04e585
zinsoftstudios <zinsoftstudios@gmail.com>
2021-03-07 11:14:52 -0600

    Forgotten uncomitted changes.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hello_world.urls')),
     path('projects/', include('projects.urls')),
+    path('blog/', include('blog.urls')),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from projects.models import Projects
+from datetime import datetime
 
 # Create your views here.
 
 def project_index(request):
     projects = Projects.objects.all()
     context = {
-            'projects': projects
+            'projects': projects,
+            'year':datetime.now().year,
         }
     return render(request, 'project_index.html', context)
 
 def project_details(request, pk):
     project = Projects.objects.get(pk = pk)
     context = {
-            'project': project
+            'project': project,
+            'year':datetime.now().year,
         }
     return render(request, 'project_details.html', context)
